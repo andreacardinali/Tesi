ff06869929fd21ea4b502e325d83191e9a49536f
Andrea Cardinali <andreacardinali@gmail.com>
2022-03-13 15:37:44 +0100

    Tuned-up assembly description

--- FakePatch/Properties/AssemblyInfo.cs
+++ FakePatch/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Modificare i valori di questi attributi per modificare le informazioni
 // associate a un assembly.
-[assembly: AssemblyTitle("FakePatch")]
+[assembly: AssemblyTitle("Update")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FakePatch")]
+[assembly: AssemblyProduct("Update")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
